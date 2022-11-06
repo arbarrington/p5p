55868c86b0cf27cf2e769fcd5e10113c8565b24e
AustinBarrington <arbarrington12@gmail.com>
2022-11-06 09:04:14 -0700

    checkout skeleton and props passed

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -20,7 +20,7 @@ export function Cart ({cartOpen, setCartOpen, cart, setCart}) {
         <div className="ms-auto fw-bold fs-5">
           Total{" "}${cart.map((item)=>{return item.product.price*item.quantity}).reduce((partialSum, a)=>partialSum+a,0)}
         </div>
-        <Button onClick={()=>navigate('/checkout')}>Checkout</Button>
+        <Button onClick={()=>{navigate('/checkout');setCartOpen(false)}}>Checkout</Button>
         </Stack>
       </Offcanvas>
     )
--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -1,10 +1,20 @@
-
+import {Col, Row} from 'react-bootstrap'
 
 export function Checkout ({cart}) {
-  console.log('checkout', cart)
-  return (
-    cart.map((item)=>{
-      <h1>{item.product.name}</h1>
-    })
-  )
+  console.log('checkout', cart[0])
+  return (<>
+  <h1>I'm the checkout</h1>
+  <Row>
+    <Col>Product Name</Col>
+    <Col>Quantity Ordered</Col>
+    <Col>Price per Unit</Col>
+    <Col>Total</Col>
+  </Row>
+    {cart.map((item)=>{return <Row>
+     <Col>{item.product.name}</Col>
+     <Col>{item.quantity} {item.product.unit}</Col>
+     <Col>{item.product.price}</Col>
+     <Col>{item.quantity * item.product.price}</Col>
+     </Row>})}
+ </> )
 }
