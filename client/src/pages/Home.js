e2c52137fdb81bd71f6303cb7ea7001c12c7741e
AustinBarrington <arbarrington12@gmail.com>
2022-11-14 14:03:45 -0700

    farmer orders coming through

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,20 +1,32 @@
 import {useEffect, useState} from 'react'
 
 export function Home ({user}) {
-  const [orders,setOrders] = useState([])
+  const [ordersIn,setOrdersIn] = useState([])
+  const [ordersOut,setOrdersOut] = useState([])
+  
   const fetchUserData = ()=> fetch(`/users/${user.username}`)
-  .then(r=>r.json()).then(data=>{ setOrders(data.orders) })
-  // eslint-disable-next-line
-  useEffect(() => { fetchUserData() }, [user.username])
-  console.log(orders)
+  .then(r=>r.json()).then(data=>{ setOrdersIn(data.orders) })
+
+  const fetchOrderData = ()=> fetch(`/orders`)
+  .then(r=>r.json()).then(data=>{ setOrdersOut(data) })
+
+  useEffect(() => { fetchUserData(); fetchOrderData() }, [user.username])
+  console.log(ordersIn)
+
+
+
+
   return (
     <div className="">
       <h2 className="col">{user.display_name}</h2>
       <img className="icon" src={user.icon} alt="User Icon"/>
       <h3>{user.bio}</h3>
       <h1 className="">My Orders and Subscriptions</h1>
-      {orders.map((order)=>{return <>
+      {ordersIn.map((order)=>{return <>
         <h2>${order.price}---{order.delivery_address}</h2>
+        </>})}
+      {ordersOut.map((order)=>{return <>
+        <h2>{order.first_name}</h2>
         </>})}
       {!user.producer?
         <h1>Favorite Producers</h1>
